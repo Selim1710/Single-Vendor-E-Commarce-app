e2f0b7811414a819cbd66b0f0fd60e81015159c9
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-03-15 18:14:40 +0600

    design search bar and menu for desktop and mobile

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -11,30 +11,39 @@
     <!-- Style Sheet -->
     <link rel="stylesheet" href="{{ asset('website/css/style.css') }}">
 
-    <title>BGD online limited</title>
+    <title>BGD Online Limited</title>
 </head>
 
 <body>
-    <section class="navbarSection">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <section class="header">
+        <nav class="navbar navbar-expand-lg navbar-light  bg-white">
             <a class="navbar-brand" href="#"><img src="{{ url('website/images/logo.png') }}" alt="" srcset=""></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- icon -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#category" aria-controls="category" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            </form>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Offers <span class="sr-only">(current)</span></a>
+            <!-- search bar -->
+            <div class="searchBar">
+                <form class="form-inline m-2">
+                    <input class="form-control mr-sm-2 w-100" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+
+            <div class="collapse navbar-collapse mr-auto" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mt-4">
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="#">
+                            <h3>Offers</h3>
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Laptop Deal</a>
+                    <li class="nav-item p-2">
+                        <a class="nav-link" href="#">
+                            <h4>Laptop <br> Deal</h4>
+                        </a>
                     </li>
-                    <li class="nav-item dropdown">
+                    <li class="nav-item dropdown p-2">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Account
+                            <h3>Account</h3>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="#">login</a>
@@ -44,65 +53,69 @@
                 </ul>
             </div>
         </nav>
-    </section>
-    <!-- Category Section -->
-    <section class="category">
-        <div class="menu bg-white">
-            <ul>
-                <li><a href="#">Desktop</a></li>
-                <li><a href="#">Laptop</a></li>
-                <li><a href="#">Component</a></li>
-                <li><a href="#">Monitor</a></li>
-                <li><a href="#">UPS</a>
-                    <div class="sub_menu_1">
-                        <ul>
-                            <li><a href="#">Online UPS</a></li>
-                            <li><a href="#">Offline UPS</a></li>
-                            <li><a href="#">Battery</a></li>
-                            <li><a href="#">All UPS</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+        <!-- Category -->
+        <div class="category" id="category">
+            <div class="menu bg-white">
+                <ul>
+                    <li><a href="#">Desktop</a></li>
+                    <li><a href="#">Laptop</a></li>
+                    <li><a href="#">Component</a></li>
+                    <li><a href="#">Monitor</a></li>
+                    <li><a href="#">UPS</a>
+                        <div class="sub_menu_1">
+                            <ul>
+                                <li><a href="#">Online UPS</a></li>
+                                <li><a href="#">Offline UPS</a></li>
+                                <li><a href="#">Battery</a></li>
+                                <li><a href="#">All UPS</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 
     <!-- compare product -->
 
     <section class="product-slider">
-        <div class="container-fluid">
-            <div class="makeFlex">
-                <div class="carousel" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="{{ asset('website/images/t1.jpg') }}" alt="First slide">
+        <div class="container">
+            <div class="row">
+                <div class="col col-md-8">
+                    <div class="carousel" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('website/images/t1.jpg') }}" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ asset('website/images/t2.jpg') }}" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ asset('website/images/t3.jpg') }}" alt="Third slide">
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="{{ asset('website/images/t2.jpg') }}" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="{{ asset('website/images/t3.jpg') }}" alt="Third slide">
-                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
-                <div class="compare-product">
-                    <h1>Compare Product</h1>
-                    <p>Choose two product to compare</p>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <br>
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                        <br>
-                        <input type="submit" style="color:white;background-color: grey;">
-                    </form>
+                <div class="col col-md-4">
+                    <div class="compare-product">
+                        <h1>Compare Product</h1>
+                        <p>Choose two product to compare</p>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search">
+                            <br>
+                            <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search">
+                            <br>
+                            <input type="submit" style="color:white;background-color: grey;">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -336,7 +349,7 @@
             <div class="stay-connected text-center">
                 <h4>STAY CONNECTED </h4>
                 <p>BGD online limitd <br>
-                    Phone : +88 019-72379494 <br> 
+                    Phone : +88 019-72379494 <br>
                     Landline : : +88 02-8090528 <br>
                     Email : info@bgdonline.net <br>
                     Address : 820, Mamun Tower(8th floor),
@@ -345,18 +358,6 @@
             </div>
         </div>
     </section>
-
-
-
-
-
-
-
-
-
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
