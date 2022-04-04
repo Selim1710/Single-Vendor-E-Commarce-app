6e08bcf3efccf4540901505b48cb7c581ab7dba5
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-04-04 13:19:27 +0600

    update offer details page

--- resources/views/website/layouts/offer_details.blade.php
+++ resources/views/website/layouts/offer_details.blade.php
@@ -1,36 +1,53 @@
 @extends('website.master')
 @section('contents')
-<!-- featured product -->
-
-<section class="featured-Product">
-    <div class="productHeader">
-        <h1> Offer Details</h1>
-    </div>
+<!-- style="border:5px solid red;" -->
+<!-- Product Offer Details  -->
+<section class="product-offer">
     <div class="container">
-        <div class="head">
-            <p>offer ends in: </p>
-            <div class="time">
-                <p>Days: Hour Minute Seconds</p>
+        <div class="offer text-center">
+            <br><br>
+            <div class="time d-flex">
+                <span class="title text-uppercase">offers ends in</span>
+                <div class="time-details">
+                    <span class="digit">0</span>
+                    <span class="digit">1</span><br>
+                    <span class="tag pr-2">Days</span>
+                </div>
+                <div class="time-details">
+                    <span class="digit">0</span>
+                    <span class="digit">1</span><br>
+                    <span class="tag pr-2">Hour</span>
+                </div>
+                <div class="time-details">
+                    <span class="digit">1</span>
+                    <span class="digit">1</span><br>
+                    <span class="tag pr-2">Minute</span>
+                </div>
+                <div class="time-details">
+                    <span class="digit">1</span>
+                    <span class="digit">0</span><br>
+                    <span class="tag pr-2">Seconds</span>
+                </div>
+            </div>
+            <br><br>
+            <div class="offer-details">
+                <img src="{{ asset('website/images/offer-1.jpg') }}" class="w-75"><br><br>
+                <p>
+                    পবিত্র রমজান মাস উপলক্ষে স্টার টেক উত্তরা সোনারগাঁ জনপথ শাখা থেকে ল্যাপটপ বা ডেস্কটপ কিনলেই নিশ্চিত উপহার হিসেবে পাচ্ছেন ৬৫০ টাকা সমমূল্যের শপিং ভাউচার এবং টি-শার্ট সহ আকর্ষনীয় নিশ্চিত উপহার।
+
+                    এছাড়াও যে কোন পণ্য কিনলেই সাথে থাকছে একটি লাকি কুপন যেখানে ১ম পুরষ্কার হিসেবে থাকছে ল্যাপটপ, ২য় পুরষ্কার হিসেবে থাকছে একটি মনিটর এবং ৩য় পুরষ্কার হিসেবে থাকছে একটি ২ঃ১ স্পিকার।
+
+                    *ঈদের আগের রাতে র‍্যাফেল ড্রে এর মাধ্যমে ভাগ্যবান বিজয়ীদের নাম ঘোষনা করা হবে।
+
+                </p>
+                <span class="text-secondary">
+                    শর্তাবলীঃ
+
+                    ক্রেতাগন এই অফারটি শুধুমাত্র স্টার টেক উত্তরা সোনারগাঁ জনপথ শাখা থেকে উপভোগ করতে পারবেন।
+                    অফারটি ৩ এপ্রিল - ১৩ই এপ্রিল পর্যন্ত কার্যকর থাকবে।
+                </span>
             </div>
         </div>
-        <div class="offer-details">
-            <img src="{{ asset('website/images/offer-1.jpg') }}" alt="" class="img-fluid"><br><br>
-            <p>
-                পবিত্র রমজান মাস উপলক্ষে স্টার টেক উত্তরা সোনারগাঁ জনপথ শাখা থেকে ল্যাপটপ বা ডেস্কটপ কিনলেই নিশ্চিত উপহার হিসেবে পাচ্ছেন ৬৫০ টাকা সমমূল্যের শপিং ভাউচার এবং টি-শার্ট সহ আকর্ষনীয় নিশ্চিত উপহার।
-
-                এছাড়াও যে কোন পণ্য কিনলেই সাথে থাকছে একটি লাকি কুপন যেখানে ১ম পুরষ্কার হিসেবে থাকছে ল্যাপটপ, ২য় পুরষ্কার হিসেবে থাকছে একটি মনিটর এবং ৩য় পুরষ্কার হিসেবে থাকছে একটি ২ঃ১ স্পিকার।
-
-                *ঈদের আগের রাতে র‍্যাফেল ড্রে এর মাধ্যমে ভাগ্যবান বিজয়ীদের নাম ঘোষনা করা হবে।
-
-            </p>
-            <span class="text-secondary">
-                শর্তাবলীঃ
-
-                ক্রেতাগন এই অফারটি শুধুমাত্র স্টার টেক উত্তরা সোনারগাঁ জনপথ শাখা থেকে উপভোগ করতে পারবেন।
-                অফারটি ৩ এপ্রিল - ১৩ই এপ্রিল পর্যন্ত কার্যকর থাকবে।
-            </span>
-        </div>
-
 
     </div>
 </section>
