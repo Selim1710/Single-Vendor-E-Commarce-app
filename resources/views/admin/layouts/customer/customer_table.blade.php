771e19ac7e864c438496cd06a3d0d511a7951002
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-05-21 17:09:00 +0600

    data table integration

--- resources/views/admin/layouts/customer/customer_table.blade.php
+++ resources/views/admin/layouts/customer/customer_table.blade.php
@@ -13,14 +13,14 @@
 @endif
 <!-- end -->
 <div class="manage_table">
-    <table class="table table-borderless table-hover">
+    <table class="table table-borderless table-hover" id="customer_table">
         <thead class="table-primary">
             <tr class="text-center">
                 <th>Name</th>
                 <th>email</th>
                 <th>phone</th>
                 <th>Address</th>
-                <th>Disable</th>
+                <th class="bg-warning"><span class="text-success">Inable</span> / <span class="text-danger">Disable</span></th>
             </tr>
         </thead>
         <tbody>
@@ -43,3 +43,12 @@
     </table>
 </div>
 @endsection
+
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js" integrity="sha512-BkpSL20WETFylMrcirBahHfSnY++H2O1W+UnEEO4yNIl+jI2+zowyoGJpbtk6bx97fBXf++WJHSSK2MV4ghPcg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    $(document).ready(function() {
+        $('#customer_table').DataTable();
+    });
+</script>
